f86f0f4dbca3c55dc687739f9e6ec898da62a452
rochafialvin <rochafi.dev@gmail.com>
2021-12-09 16:01:41 +0700

    Setup NewsAPI

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,3 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({baseURL: 'http://10.0.2.2:2021'});
+export const apiNews = axios.create({
+  baseURL: 'https://newsapi.org',
+});
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import {Box, Text, Button} from 'native-base';
+import React, {useState} from 'react';
+import {Box, Text, Button, Input} from 'native-base';
+import {apiNews} from '../../config/axios';
+
+import AuthInput from '../../components/AuthInput';
 
 export default function HomeScreen({navigation}) {
+  const [keyword, setKeyword] = useState('');
+
+  const fetchNews = async () => {
+    try {
+      const res = await apiNews.get('/v2/everything', {
+        params: {
+          apiKey: 'c7a94f6e229f477a9a5471f8125fa296',
+          q: keyword,
+          language: 'id',
+          pageSize: 5,
+          page: 1,
+        },
+      });
+
+      console.log(res.data);
+    } catch (error) {
+      alert('Terjadi kesalahan');
+    }
+  };
+
   return (
     <Box>
       <Text> Home Screen </Text>
-      <Button onPress={() => navigation.navigate('Detail')}>
-        Go to Detail
-      </Button>
+      <AuthInput onChangeText={setKeyword} />
+      <Button onPress={fetchNews}>Search</Button>
     </Box>
   );
 }
