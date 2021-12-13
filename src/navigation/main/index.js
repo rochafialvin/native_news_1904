9ee417c079a6ec739dd49a61ad1817a274ca799e
rochafialvin <rochafi.dev@gmail.com>
2021-12-13 16:15:51 +0700

    Setup custom icon in navigator component

--- src/navigation/main/index.js
+++ src/navigation/main/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-
+import {Text} from 'native-base';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import HomeStackNavigation from '../home';
@@ -10,34 +10,35 @@ const {Navigator, Screen} = createMaterialBottomTabNavigator();
 
 export default function MainNavigation() {
   return (
-    <Navigator initialRouteName="HomeStack">
+    <Navigator
+      initialRouteName="HomeStack"
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color}) => {
+          let iconName;
+
+          // name yang kita gunakan pada component screen dibawah
+          switch (route.name) {
+            case 'HomeStack':
+              iconName = focused ? 'home' : 'home-outline';
+              break;
+
+            case 'ProfileStack':
+              iconName = focused ? 'account' : 'account-outline';
+              break;
+          }
+
+          return <Icon name={iconName} color={color} size={25} />;
+        },
+      })}>
       <Screen
         name="HomeStack"
         component={HomeStackNavigation}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({focused}) => (
-            <Icon
-              name={focused ? 'home' : 'home-outline'}
-              color={'#ffffff'}
-              size={25}
-            />
-          ),
-        }}
+        options={{tabBarLabel: 'Home'}}
       />
       <Screen
         name="ProfileStack"
         component={ProfileStackNavigation}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({focused}) => (
-            <Icon
-              name={focused ? 'account' : 'account-outline'}
-              color={'#ffffff'}
-              size={25}
-            />
-          ),
-        }}
+        options={{tabBarLabel: 'Profile'}}
       />
     </Navigator>
   );
