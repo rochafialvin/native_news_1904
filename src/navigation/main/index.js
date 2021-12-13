aa46e43d20e82417ccec3cf42fe5c464fcb02dbd
rochafialvin <rochafi.dev@gmail.com>
2021-12-13 15:19:47 +0700

    Use Custom icon on tab navigator based on focused value

--- src/navigation/main/index.js
+++ src/navigation/main/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import HomeStackNavigation from '../home';
 import ProfileStackNavigation from '../profile';
 
@@ -9,8 +11,34 @@ const {Navigator, Screen} = createMaterialBottomTabNavigator();
 export default function MainNavigation() {
   return (
     <Navigator initialRouteName="HomeStack">
-      <Screen name="HomeStack" component={HomeStackNavigation} />
-      <Screen name="ProfileStack" component={ProfileStackNavigation} />
+      <Screen
+        name="HomeStack"
+        component={HomeStackNavigation}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({focused}) => (
+            <Icon
+              name={focused ? 'home' : 'home-outline'}
+              color={'#ffffff'}
+              size={25}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="ProfileStack"
+        component={ProfileStackNavigation}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({focused}) => (
+            <Icon
+              name={focused ? 'account' : 'account-outline'}
+              color={'#ffffff'}
+              size={25}
+            />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
