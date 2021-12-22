bc74449099696f033ff255327eea514408ac3ace
rochafialvin <rochafi.dev@gmail.com>
2021-12-22 09:41:08 +0700

    Update axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export default axios.create({baseURL: 'http://10.0.2.2:2021'});
+// export default axios.create({baseURL: 'http://10.0.2.2:2021'});
+export default axios.create({baseURL: 'https://flash-news-api.herokuapp.com'});
 export const apiNews = axios.create({
   baseURL: 'https://newsapi.org',
 });
